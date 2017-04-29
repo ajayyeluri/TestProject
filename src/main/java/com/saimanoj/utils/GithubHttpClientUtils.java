a55c35e6b867502e9802b15a048681b4d1761958
shrimanoz <saimanu.manoj@gmail.com>
2017-04-29 13:09:20 -0400

    new method

--- src/main/java/com/saimanoj/utils/GithubHttpClientUtils.java
+++ src/main/java/com/saimanoj/utils/GithubHttpClientUtils.java
@@ -1,5 +1,7 @@
 package com.saimanoj.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saimanoj.model.Commit;
 import com.saimanoj.model.Stats;
 import org.springframework.core.ParameterizedTypeReference;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -18,7 +21,7 @@ public class GithubHttpClientUtils {
 
     private String OwnerName;
     private String RepoName;
-    private List<Commit> shaList;
+
 
     public String getOwnerName() {
         return OwnerName;
@@ -48,21 +51,49 @@ public class GithubHttpClientUtils {
                 restTemplate.exchange(url,
                         HttpMethod.GET, null, new ParameterizedTypeReference<List<Commit>>() {
                         });
-        List<Commit> shaList =  rateResponse.getBody();
-        this.shaList = shaList;
-        return shaList;
+        return rateResponse.getBody();
 
     }
 
-    public Stats getCommitDetails(Commit commit){
-        String url = "https://api.github.com/repos/"+OwnerName+"/"+RepoName+"/commits";
+    public Stats getCommitsDetails(String OwnerName , String RepoName){
+        GithubHttpClientUtils clientUtils = new GithubHttpClientUtils();
+        List<Commit> shaList =  clientUtils.getCommits(OwnerName , RepoName);
+        Iterator<Commit> iterator = shaList.iterator();
+        while(iterator.hasNext()) {
+            String url = "https://api.github.com/repos/" + OwnerName + "/" + RepoName + "/commits";
+            url = url + iterator.next().getSha();
+            RestTemplate restTemplate = new RestTemplate();
+            Stats stats = restTemplate
+                    .getForObject( url, Stats.class);
+            int adds = stats.getAdds();
+            int deletes = stats.getDeletes();
+            int totalChanges = stats.getTotalChanges();
+            String sha = stats.getSha();
+            Date commitDate = stats.getCommitDate();
+        }
+       return null;
+    }
+
+   /* public Stats getCommitDetails(Commit commit){
+        String url = commit.getUrl();
+
     Iterator<Commit> iterator = shaList.iterator();
     while(iterator.hasNext()){
-        url= url+iterator.next();
+        url= url+iterator.next().getSha();
         RestTemplate restTemplate = new RestTemplate();
-       // ResponseEntity rateResponse = restTemplate.getForEntity()
+       ResponseEntity<String> restResponse =
+               restTemplate.getForEntity(url , String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(restResponse.getBody());
+        JsonNode name = root.path("name");
 
     }
-        return null ;
-    }
+    Stats stats = new Stats();
+    stats.setAdds();
+    stats.setDeletes();
+    stats.setTotalChanges();
+    stats.setCommitDate();
+        return stats ;
+    }--> */
 }
