a83820939c54c2d5eb10ee0e154a46c6ff2b52e5
shrimanoz <saimanu.manoj@gmail.com>
2017-04-29 14:51:17 -0400

    Test class updated

--- src/test/java/GithubHttpClientUtilsTest.java
+++ src/test/java/GithubHttpClientUtilsTest.java
@@ -1,11 +1,15 @@
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.saimanoj.model.Commit;
 
+import com.saimanoj.model.Stats;
+import com.saimanoj.utils.GithubHttpClientUtils;
 import org.junit.Test;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
@@ -17,24 +21,25 @@ import static org.junit.Assert.assertNotNull;
 
 public class GithubHttpClientUtilsTest {
 
-
+    GithubHttpClientUtils test = new GithubHttpClientUtils();
+    Commit commit = new Commit();
     //test case for getCommits method
     @Test
     public void getCommitsTest(){
-        String OwnerName = "shrimanoz";
-        String RepoName = "TestProject";
-        //Checking for Url
-        String url = "https://api.github.com/repos/"+OwnerName+"/"+RepoName+"/commits";
-        // Get Json Response
-        RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<List<Commit>> rateResponse =
-                restTemplate.exchange(url,
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Commit>>() {
-                        });
-
-        assertNotNull( rateResponse.getBody());
+        List<Commit> list =  test.getCommits("shrimanoz" , "TestProject");
+        for(int i=0;i<list.size();i++){
+            Commit commit = list.get(i);
+            System.out.println("The Url is :"+commit.getUrl()+ "The Sha Value is :" +commit.getSha());
+        }
 
     }
+    @Test
+    public void getcommitdetails(){
+        Stats testStas = test.getCommitsDetails("shrimanoz" , "TestProject" , "c8f978b8486234e84a1d4bb23109bca611a61859" );
+        System.out.println(testStas.toString());
+    }
+
+
 
 }
